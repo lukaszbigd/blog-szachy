d7197f742b87f91044e534f6b7a34bb5fd05c3b6
Siwy21372115 <126457727+Siwy21372115@users.noreply.github.com>
2023-03-10 12:14:36 +0100

    wyswietlanie update

--- wyswietlanie.php
+++ wyswietlanie.php
@@ -15,7 +15,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
-    echo '<img src="data:image/jpeg;base64,'.base64_encode($row['IMAGE']).'" / >';
+    echo '<img src="data:image/jpeg;base64,'.base64_encode($row['image_data']).'" / >';
     echo '<h2>'.$row['title'].'</h2>';
     echo '<p>'.$row['text'].'</p>';
   
