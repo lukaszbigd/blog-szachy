2fefa539ddd74324073654db015191465f20830f
Siwy21372115 <126457727+Siwy21372115@users.noreply.github.com>
2023-03-17 11:55:23 +0100

    admin loged page

---
+++ admin2.php
@@ -0,0 +1,44 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Debiutex</title>
+    <link rel="stylesheet" type="text/css" href="./assets/main_ind_style.css">
+</head>
+
+<body>
+    <header>
+        <div class="list">
+            <p>-</p>
+            <div class="dropdown">
+                <a href="log.php">Panel administratora</a>
+            </div>
+        </div>
+        <h1 class="headline">Blog o tematyce szachów</h1>
+    </header>
+    <main>
+        <?php
+        require_once "deletewpis.php"
+            ?>
+    </main>
+    <script>
+        var list = document.querySelector('.list');
+        var dropdown = document.querySelector('.dropdown');
+        list.addEventListener('mouseover', function () {
+            dropdown.style.display = 'block';
+        });
+        list.addEventListener('mouseout', function () {
+            dropdown.style.display = 'none';
+        });
+        var submit = document.getElementsByClassName('submit');
+        const sumbit = document.getElementsByClassName('submit');
+        submit.addEventListener("click", function (event) {
+            event.preventdefault()
+            console.log(submit.getAttribute('name'));
+        });
+
+    </script>
+</body>
+
+
+</html>
